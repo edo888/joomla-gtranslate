5dd103b9ff1ced1c957fcd0a5e2620a449fc74e9
edo888 <edo888@10e4280c-dc07-11dd-a297-4fb579711a53>
2012-03-14 22:42:45 +0000

    j15 fix

--- trunk/mod_gtranslate16/headbar/gtranslateheadbar.php
+++ trunk/mod_gtranslate16/headbar/gtranslateheadbar.php
@@ -12,28 +12,28 @@ jimport('joomla.form.formfield');
 
 class JFormFieldGTranslateHeadbar extends JFormField {
 
-	protected $type = 'GTranslateHeadbar';
+    protected $type = 'GTranslateHeadbar';
 
-	public function getLabel() {
-		return;
-	}
+    public function getLabel() {
+        return;
+    }
 
-	protected function getInput() {
-		$html = '<div style="float:left;padding:10px 0 0 10px"><a href="http://gtranslate.net/" target="_blank"><img src="http://gtranslate.net/templates/gtranslate/images/gt_logo_joomla.png" alt="GTranslate"></a></div>
+    protected function getInput() {
+        $html = '<div style="float:left;padding:10px 0 0 10px"><a href="http://gtranslate.net/" target="_blank"><img src="http://gtranslate.net/templates/gtranslate/images/gt_logo_joomla.png" alt="GTranslate"></a></div>
 <div class="toolbar">
-	<table class="toolbar">
-	<tr>
-		<td class="button">
-			<a class="toolbar" rel="help" onclick="popupWindow(\'http://gtranslate.net/documentation/mod_gtranslate_j16\', \'Documentation\', 700, 500, 1)" href="#">
-			<span class="icon-32-help"></span>Documentation</a>
-		</td>
-	</tr>
-	</table>
+    <table class="toolbar">
+    <tr>
+        <td class="button">
+            <a class="toolbar" rel="help" onclick="popupWindow(\'http://gtranslate.net/docs/54-gtranslate-module-documentation?tmpl=component\', \'Documentation\', 700, 500, 1)" href="#">
+            <span class="icon-32-help"></span>Documentation</a>
+        </td>
+    </tr>
+    </table>
 </div>
 <div class="clr"></div>';
 
         //TODO: update notification <iframe width="1" height="1" frameborder="0" src=""></iframe>
 
-		return $html;
-	}
+        return $html;
+    }
 }
