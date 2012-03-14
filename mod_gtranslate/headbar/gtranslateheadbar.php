1ad1e2e1ccea46ca575ea199c1b12f14cd636b5c
edo888 <edo888@10e4280c-dc07-11dd-a297-4fb579711a53>
2012-03-14 22:42:45 +0000

    j15 fix

--- mod_gtranslate/headbar/gtranslateheadbar.php
+++ mod_gtranslate/headbar/gtranslateheadbar.php
@@ -10,28 +10,28 @@ defined('_JEXEC') or die('Restricted access');
 
 class JElementGTranslateHeadbar extends JElement {
 
-	var	$_name = 'GTranslateHeadbar';
+    var $_name = 'GTranslateHeadbar';
 
-	function fetchTooltip() {
-		return;
-	}
+    function fetchTooltip() {
+        return;
+    }
 
-	function fetchElement($name, $value, &$node) {
-		$html = '<div style="float:left;padding:10px 0 0 10px"><a href="http://gtranslate.net/" target="_blank"><img src="http://gtranslate.net/templates/gtranslate/images/gt_logo_joomla.png" alt="GTranslate"></a></div>
+    function fetchElement($name, $value, &$node) {
+        $html = '<div style="float:left;padding:10px 0 0 10px"><a href="http://gtranslate.net/" target="_blank"><img src="http://gtranslate.net/templates/gtranslate/images/gt_logo_joomla.png" alt="GTranslate"></a></div>
 <div class="toolbar">
-	<table class="toolbar">
-	<tr>
-		<td class="button">
-			<a class="toolbar" rel="help" onclick="popupWindow(\'http://gtranslate.net/documentation/mod_gtranslate_j15\', \'Documentation\', 700, 500, 1)" href="#">
-			<span class="icon-32-help"></span>Documentation</a>
-		</td>
-	</tr>
-	</table>
+    <table class="toolbar">
+    <tr>
+        <td class="button">
+            <a class="toolbar" rel="help" onclick="popupWindow(\'http://gtranslate.net/docs/54-gtranslate-module-documentation?tmpl=component\', \'Documentation\', 700, 500, 1)" href="#">
+            <span class="icon-32-help"></span>Documentation</a>
+        </td>
+    </tr>
+    </table>
 </div>
 <div class="clr"></div>';
 
         //TODO: update notification <iframe width="1" height="1" frameborder="0" src=""></iframe>
 
-		return $html;
-	}
+        return $html;
+    }
 }
