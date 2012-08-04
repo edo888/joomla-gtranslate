40d933bcddc6b8dffdd009370e4f60e9b305b7b5
edo888 <edo888@10e4280c-dc07-11dd-a297-4fb579711a53>
2012-08-04 13:56:32 +0000

    js packed

--- trunk/mod_gtranslate16/tmpl/default.php
+++ trunk/mod_gtranslate16/tmpl/default.php
@@ -74,22 +74,7 @@ if(!defined('GTRANSLATE_INCLUDED')) {
 <?php if($method == 'onfly'): ?>
 <script type="text/javascript">
 /* <![CDATA[ */
-function GTranslateFireEvent(element, event) {try {if (document.createEventObject){var evt = document.createEventObject();element.fireEvent('on'+event,evt);} else {var evt = document.createEvent("HTMLEvents");evt.initEvent(event, true, true );element.dispatchEvent(evt);}} catch (e) {}}
-function doGTranslate(lang_pair) {
-    if(lang_pair.value)lang_pair=lang_pair.value;if(lang_pair=='')return;var lang=lang_pair.split('|')[1];
-
-    var teCombo;
-    var sel = document.getElementsByTagName('select');
-    for(var i = 0; i < sel.length; i++)
-       if(sel[i].className == 'goog-te-combo')
-           teCombo = sel[i];
-
-    if(document.getElementById('google_translate_element2') == null || document.getElementById('google_translate_element2').innerHTML.length == 0 || teCombo.length == 0  || teCombo.innerHTML.length == 0) {setTimeout(function() { doGTranslate(lang_pair); }, 500);}
-    else {
-        teCombo.value = lang;
-        GTranslateFireEvent(teCombo,'change');GTranslateFireEvent(teCombo,'change');
-    }
-}
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 7(a,b){n{4(2.9){3 c=2.9();a.o(\'p\'+b,c)}f{3 c=2.q("r");c.s(b,g,g);a.t(c)}}u(e){}}6 h(a){4(a.8)a=a.8;4(a==\'\')v;3 b=a.w(\'|\')[1];3 c;3 d=2.x(\'y\');z(3 i=0;i<d.5;i++)4(d[i].A==\'B-C-D\')c=d[i];4(2.j(\'k\')==E||2.j(\'k\').l.5==0||c.5==0||c.l.5==0){F(6(){h(a)},G)}f{c.8=b;7(c,\'m\');7(c,\'m\')}}',43,43,'||document|var|if|length|function|GTranslateFireEvent|value|createEventObject||||||else|true|doGTranslate||getElementById|google_translate_element2|innerHTML|change|try|fireEvent|on|createEvent|HTMLEvents|initEvent|dispatchEvent|catch|return|split|getElementsByTagName|select|for|className|goog|te|combo|null|setTimeout|500'.split('|'),0,{}))
 /* ]]> */
 </script>
 <?php endif; ?>
