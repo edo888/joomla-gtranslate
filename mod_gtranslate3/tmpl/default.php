8af100f3289b9974ddc9d9ba8b2cf39a4a9d4efb
edo888 <edo888@gmail.com>
2017-03-06 08:35:26 -0500

    version 3.7.2

--- mod_gtranslate3/tmpl/default.php
+++ mod_gtranslate3/tmpl/default.php
@@ -73,6 +73,9 @@ if(!defined('GTRANSLATE_INCLUDED')) {
 function GTranslateGetCurrentLang() {var keyValue = document.cookie.match('(^|;) ?googtrans=([^;]*)(;|$)');return keyValue ? keyValue[2].split('/')[2] : null;}
 function GTranslateFireEvent(element,event){try{if(document.createEventObject){var evt=document.createEventObject();element.fireEvent('on'+event,evt)}else{var evt=document.createEvent('HTMLEvents');evt.initEvent(event,true,true);element.dispatchEvent(evt)}}catch(e){}}
 function doGTranslate(lang_pair){if(lang_pair.value)lang_pair=lang_pair.value;if(lang_pair=='')return;var lang=lang_pair.split('|')[1];if(GTranslateGetCurrentLang() == null && lang == lang_pair.split('|')[0])return;<?php if($analytics): ?>if(typeof ga!='undefined'){ga('send', 'event', 'GTranslate', lang, location.hostname+location.pathname+location.search);}else{if(typeof _gaq!='undefined')_gaq.push(['_trackEvent', 'GTranslate', lang, location.hostname+location.pathname+location.search]);}<?php endif; ?>var teCombo;var sel=document.getElementsByTagName('select');for(var i=0;i<sel.length;i++)if(sel[i].className=='goog-te-combo')teCombo=sel[i];if(document.getElementById('google_translate_element2')==null||document.getElementById('google_translate_element2').innerHTML.length==0||teCombo.length==0||teCombo.innerHTML.length==0){setTimeout(function(){doGTranslate(lang_pair)},500)}else{teCombo.value=lang;GTranslateFireEvent(teCombo,'change');GTranslateFireEvent(teCombo,'change')}}
+<?php if($look == 'dropdown_with_flags'): ?>
+if(GTranslateGetCurrentLang() != null)jQuery(document).ready(function() {jQuery('div.switcher div.selected a').html(jQuery('div.switcher div.option').find('img[alt="'+GTranslateGetCurrentLang()+'"]').parent().html());});
+<?php endif; ?>
 /* ]]> */
 </script>
 <?php
@@ -200,7 +203,7 @@ if($look == 'flags' or $look == 'flags_name' or $look == 'flags_code' or $look =
             echo '<option '.($lang == $language ? 'style="font-weight:bold;"' : '').' value="'.$language.'|'.$lang.'">'.$lang_name.'</option>';
     }
     echo '</select>';
-} elseif ($look = 'dropdown_with_flags') {
+} elseif ($look == 'dropdown_with_flags') {
 
     JHtml::_('jquery.framework');
 
