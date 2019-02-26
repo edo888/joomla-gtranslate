7e2d5bc4d2bde615cf413e85b7fcc8068f41be92
edo888 <edo888@gmail.com>
2019-02-26 08:17:48 -0500

    live chat issue fixed

--- mod_gtranslate3/field/livechat.php
+++ mod_gtranslate3/field/livechat.php
@@ -29,7 +29,7 @@ class JFormFieldLiveChat extends JFormField {
         $website = addslashes(JURI::root());
 
         $live_chat = <<<EOM
-        <script>window.intercomSettings = {app_id: "r70azrgx", hide_default_launcher: false, 'email': '{$user->email}', 'name': '{$name}', 'website': '{$website}', 'platform': 'joomla'};(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/r70azrgx';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+        <script>window.intercomSettings = {app_id: "r70azrgx", hide_default_launcher: false, 'name': '{$name}', 'website': '{$website}', 'platform': 'joomla'};(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/r70azrgx';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 EOM;
         return $live_chat;
     }
